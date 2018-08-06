14f555ed186e6f0fbc2d479c9f8bbf7a1f357d05
Raveen Vijayan <raveen.vijayan@digital.hmrc.gov.uk>
2018-08-06 12:35:23 +0100

    add repository tests

--- app/uk/gov/hmrc/agentmappingfrontend/repository/MappingArnRepository.scala
+++ app/uk/gov/hmrc/agentmappingfrontend/repository/MappingArnRepository.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.agentmappingfrontend.repository
 import java.util.UUID
 
 import javax.inject.{Inject, Singleton}
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone}
 import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoComponent
 import reactivemongo.api.indexes.{Index, IndexType}
@@ -32,10 +32,15 @@ import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
 
 import scala.concurrent.{ExecutionContext, Future}
 
-case class MappingArnResult(id: MappingArnResultId, arn: Arn, createdDate: DateTime = DateTime.now)
+case class MappingArnResult(id: MappingArnResultId, arn: Arn, createdDate: DateTime = DateTime.now(DateTimeZone.UTC))
 
 object MappingArnResult {
   type MappingArnResultId = String
+
+  def apply(arn: Arn): MappingArnResult = {
+    val id: MappingArnResultId = UUID.randomUUID().toString.replace("-", "")
+    MappingArnResult(id, arn)
+  }
 
   implicit val dateFormat = ReactiveMongoFormats.dateTimeFormats
   implicit val format = Json.format[MappingArnResult]
@@ -64,8 +69,8 @@ class MappingArnRepository @Inject()(appConfig: AppConfig, mongoComponent: React
     find("id" -> id).map(_.headOption.map(_.arn))
 
   def create(arn: Arn)(implicit ec: ExecutionContext): Future[MappingArnResultId] = {
-    val id: MappingArnResultId = UUID.randomUUID().toString.replace("-", "")
-    insert(MappingArnResult(id, arn)).map(_ => id)
+    val record = MappingArnResult(arn)
+    insert(record).map(_ => record.id)
   }
 
   def delete(id: MappingArnResultId)(implicit ec: ExecutionContext): Future[Unit] =
