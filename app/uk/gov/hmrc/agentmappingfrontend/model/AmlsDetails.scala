06334517c33469279abb166fad0589389cc0ad6c
geoffwatson <9043337+geoffreywatson@users.noreply.github.com>
2021-08-17 09:53:20 +0100

    [APB-5283][GW] change SJR model to allow safeId to be stored for amls; fix broken radios

--- app/uk/gov/hmrc/agentmappingfrontend/model/AmlsDetails.scala
+++ app/uk/gov/hmrc/agentmappingfrontend/model/AmlsDetails.scala
@@ -21,7 +21,13 @@ import java.time.format.DateTimeFormatter
 
 import play.api.libs.json.{Format, JsObject, JsResult, JsSuccess, JsValue, Json, OFormat}
 
-case class RegisteredDetails(membershipNumber: String, membershipExpiresOn: LocalDate)
+case class RegisteredDetails(
+  membershipNumber: String,
+  membershipExpiresOn: LocalDate,
+  amlsSafeId: Option[String],
+  agentBPRSafeId: Option[String]) {
+  val safeIdsMatch: Option[Boolean] = amlsSafeId.flatMap(amls => agentBPRSafeId.map(_ == amls))
+}
 
 object RegisteredDetails {
   implicit val format: OFormat[RegisteredDetails] = Json.format
@@ -45,11 +51,18 @@ object AmlsDetails {
 
       val mayBeMembershipNumber = (json \ "membershipNumber").asOpt[String]
 
+      val amlsSafeId = (json \ "amlsSafeId").asOpt[String]
+
+      val agentBPRSafeId = (json \ "agentBPRSafeId").asOpt[String]
+
       mayBeMembershipNumber match {
 
         case Some(membershipNumber) =>
           val membershipExpiresOn = LocalDate.parse((json \ "membershipExpiresOn").as[String], formatter)
-          JsSuccess(AmlsDetails(supervisoryBody, Right(RegisteredDetails(membershipNumber, membershipExpiresOn))))
+          JsSuccess(
+            AmlsDetails(
+              supervisoryBody,
+              Right(RegisteredDetails(membershipNumber, membershipExpiresOn, amlsSafeId, agentBPRSafeId))))
 
         case None =>
           val appliedOn = LocalDate.parse((json \ "appliedOn").as[String], formatter)
