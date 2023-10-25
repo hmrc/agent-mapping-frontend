d0cc9c99191d87d7278878d977c525d84a08a8db
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-10-25 15:50:13 +0100

    APB-7412[CS] fix back link welsh translation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVer: String = "7.21.0"
+  private val bootstrapVer: String = "7.22.0"
   private val mongoVer: String = "1.3.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-28" % bootstrapVer,
     "com.typesafe.play"  %% "play-json-joda"             % "2.9.2",
-    "uk.gov.hmrc"        %% "play-frontend-hmrc"         % "7.19.0-play-28",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc"         % "7.23.0-play-28",
     "uk.gov.hmrc"        %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"         % mongoVer,
-    "uk.gov.hmrc"        %% "agent-mtd-identifiers"      % "1.13.0",
+    "uk.gov.hmrc"        %% "agent-mtd-identifiers"      % "1.14.0",
     "uk.gov.hmrc"        %% "agent-kenshoo-monitoring"   % "5.5.0",
     "com.github.blemale" %% "scaffeine"                  % "5.2.1"
   )
