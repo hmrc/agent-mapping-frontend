80c76c2b92a06a7dac30ec7163298902cac7356d
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-07-08 17:12:46 +0100

    APB-9617 fixing when to show signout links and timeouts

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,6 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVer,
     "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.7.0",
-    "uk.gov.hmrc"        %% "play-partials-play-30"      % "10.0.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"      % "2.2.0",
     "com.github.blemale" %% "scaffeine"                  % "5.3.0"
