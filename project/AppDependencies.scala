42ae9b7c4182de702fa77c4a834cbb8d3d20c95b
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2025-08-26 08:20:22 +0100

    [WG][APB-8225] remove agent-mtd-identifiers, update versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVer: String = "9.13.0"
-  private val mongoVer: String = "2.6.0"
+  private val bootstrapVer: String = "9.19.0"
+  private val mongoVer: String = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVer,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
     "uk.gov.hmrc"        %% "domain-play-30"            % "11.0.0",
     "com.github.blemale" %% "scaffeine"                  % "5.3.0"
