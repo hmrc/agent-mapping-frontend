9c7822f36e8b0c5121f40462348011f790337c4f
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-09-02 09:30:52 +0100

    APB-9856 revert hmrc-domain to working version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVer,
     "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
-    "uk.gov.hmrc"        %% "domain-play-30"            % "13.0.0",
+    "uk.gov.hmrc"        %% "domain-play-30"            % "11.0.0",
     "com.github.blemale" %% "scaffeine"                  % "5.3.0"
   )
 
